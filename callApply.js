11c39d669042be7b5248b63a12ce2267cc87196b
mehedi hasan <mehedihasanibnc@gmail.com>
2021-02-04 22:55:53 +0600

    bind janlam

--- callApply.js
+++ callApply.js
@@ -6,11 +6,32 @@ const normalPerosn = {
         console.log(this.firstName, this.lastName)
     },
     chargeBill: function(amount){
+        console.log(this)
         this.salary = this.salary - amount
         return this.salary
     }
 }
 
-normalPerosn.chargeBill(150)
-normalPerosn.chargeBill(150)
-console.log(normalPerosn.salary)
+// normalPerosn.chargeBill(150)
+// normalPerosn.chargeBill(150)
+// console.log(normalPerosn.salary)
+
+const heroPerson = {
+    firstName: 'Hero',
+    lastName: 'Alom',
+    salary: 30000
+}
+
+const friendlyPerson = {
+    firstName: 'Hero',
+    lastName: 'Golom',
+    salary: 30000
+}
+
+const heroChargeBill = normalPerosn.chargeBill.bind(heroPerson)
+heroChargeBill(2000)
+console.log(heroPerson.salary)
+
+const friendlyChargeBill = normalPerosn.chargeBill.bind(friendlyPerson)
+friendlyChargeBill(2500)
+console.log(friendlyPerson.salary)
